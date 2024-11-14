ff9a290460c29352f48f02132a72fc910e61618d
Nayar Joolfoo <nayar@joolfoo.com>
2024-11-14 19:17:48 +0400

    added variable in links

--- src/App.js
+++ src/App.js
@@ -9,14 +9,19 @@ function App() {
       <header>
         <ul>
           <li><Link to="/">Home</Link></li>
-          <li><Link to="/project">Project</Link></li>
+          <li><Link to="/project">Projects</Link></li>
+          <li><Link to="/project/react">Project: React</Link></li>
+          <li><Link to="/project/javascript">Project: Javascript</Link></li>
+          <li><Link to="/project/python">Project: Python</Link></li>
           <li><Link to="/about">About</Link></li>
+          <li><Link to="/h1">h1</Link></li>
         </ul>
       </header>
       <Routes>
         <Route exact path="/" element={<Home/>}/>
-        <Route exact path="/project" element={<Projects/>}/>
+        <Route exact path="/project/:name" element={<Projects/>}/>
         <Route exact path="/about" element={<About/>}/>
+        <Route exact path="/h1" element={<h1>Hello</h1>}/>
       </Routes>
     </div>
   );
