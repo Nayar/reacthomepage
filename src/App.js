9f1013116c5947b6258ac8a907e549268e19c55c
Nayar Joolfoo <nayar@joolfoo.com>
2024-11-14 18:55:03 +0400

    added link to header items

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import Home from "./Home";
 import About from "./About";
 import Projects from "./Projects";
+import {Route, Link} from "react-router-dom"
 
 function App() {
   return (
     <div>
       <header>
         <ul>
-          <li>Home</li>
-          <li>Project</li>
-          <li>About</li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/project">Project</Link></li>
+          <li><Link to="/about">About</Link></li>
         </ul>
       </header>
       <Home/>
