428039abb1a68432a89b739561c6817e084d40d8
Nayar Joolfoo <nayar@joolfoo.com>
2024-11-14 18:31:17 +0400

    add components

---
+++ src/Projects.js
@@ -0,0 +1,13 @@
+function Projects() {
+    return (
+        <div>
+            <h1>Here is a list of projects</h1>
+            <img
+            src=" https://cdn.pixabay.com/photo/2017/06/10/07/18/list-2389219_1280.png"
+            alt="About"
+            width="30%" />
+        </div>
+    )
+}
+
+export default Projects
